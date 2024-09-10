28b2ab6955e4c5a40008ba4045bd97c66a023368
عبد الله محمد عطيه عبد الرحمن فيشار stu <322223631@sha.edu.eg>
2024-09-10 03:34:35 +0300

    Video1&2

---
+++ Demo2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+       
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
--- Demo2/Startup.cs
+++ Demo2/Startup.cs
@@ -16,6 +16,8 @@ namespace Demo2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,7 +27,7 @@ namespace Demo2
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
@@ -34,6 +36,11 @@ namespace Demo2
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+                endpoints.MapControllerRoute(
+                    name:"default",
+                    pattern:"{controller}/{action}"
+                    );
+
             });
         }
     }
